74caa919852cabb089193acf9e0213b6d451df79
DESKTOP-8AK2050\刘 <lrh870968211@126.com>
2018-04-19 11:11:25 +0800

    打印hello

---
+++ demo1/src/com/stareatm/app/Hello.java
@@ -0,0 +1,10 @@
+package com.stareatm.app;
+
+/**
+ * Created by 刘 on 2018/4/19.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("打印hello");
+    }
+}
